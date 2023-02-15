98613138a1d685e3fc1ab4e71ea105ceb24c92bc
Juma Allan <allanjuma@gmail.com>
2023-02-15 21:40:08 +0300

    added copyright on kts files

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,5 +53,11 @@ subprojects {
                 "^(package|object|import|interface)"
             )
         }
+        format("kts") {
+            target("**/*.kts")
+            targetExclude("**/build/**/*.kts")
+            // Look for the first line that doesn't have a block comment (assumed to be the license)
+            licenseHeaderFile(rootProject.file("spotless/copyright.kts"), "(^(?![\\/ ]\\*).*$)")
+        }
     }
 }
