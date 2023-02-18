52483ef3f9eda08019f89ec4072afa1f3f8e8703
chepsi <evanschepsiror@gmail.com>
2023-02-18 15:06:30 +0300

    Fix target vs compile java version

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -9,6 +9,12 @@ java {
     targetCompatibility = JavaVersion.VERSION_11
 }
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
 dependencies {
     compileOnly(libs.android.gradlePlugin)
     compileOnly(libs.kotlin.gradlePlugin)
