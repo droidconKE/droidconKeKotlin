3101ef3660ad3f6e5e72b5c769b4a2fe5f8a7460
chepsi <evanschepsiror@gmail.com>
2023-02-21 23:21:19 +0300

    Fix UI tests

--- presentation/src/main/java/com/android254/presentation/common/components/SocialAuthButton.kt
+++ presentation/src/main/java/com/android254/presentation/common/components/SocialAuthButton.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.unit.dp
@@ -51,10 +52,10 @@ fun SocialAuthButton(
     ),
     content: @Composable RowScope.() -> Unit
 ) {
-    val containerColor = colors.containerColor(enabled).value
-    val contentColor = colors.contentColor(enabled).value
-    val shadowElevation = elevation?.shadowElevation(enabled, interactionSource)?.value ?: 0.dp
-    val tonalElevation = elevation?.tonalElevation(enabled, interactionSource)?.value ?: 0.dp
+    val containerColor = Color.Black
+    val contentColor = Color.Red
+    val shadowElevation =  0.dp
+    val tonalElevation =  0.dp
 
     Surface(
         onClick = onClick,
--- presentation/src/main/java/com/android254/presentation/sessions/components/EventDaySelectorButton.kt
+++ presentation/src/main/java/com/android254/presentation/sessions/components/EventDaySelectorButton.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -63,10 +64,15 @@ fun EventDaySelectorButton(
     ),
     content: @Composable RowScope.() -> Unit
 ) {
-    val containerColor = colors.containerColor(enabled).value
+    /*val containerColor = colors.containerColor(enabled).value
     val contentColor = colors.contentColor(enabled).value
     val shadowElevation = elevation?.shadowElevation(enabled, interactionSource)?.value ?: 0.dp
     val tonalElevation = elevation?.tonalElevation(enabled, interactionSource)?.value ?: 0.dp
+*/
+    val containerColor = Color.Black
+    val contentColor = Color.Red
+    val shadowElevation =  0.dp
+    val tonalElevation =  0.dp
 
     Surface(
         onClick = onClick,
