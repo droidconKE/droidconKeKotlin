b5b1a4430117bafe8d89233f53e945fd5484c2a7
Peter Chege <peterkagure@gmail.com>
2023-03-10 17:06:58 +0300

    Migrate Live Data to stateflow (#60) and consume it with collectWithLifecycle (#61)

--- presentation/src/main/java/com/android254/presentation/sessions/view/SessionsViewModel.kt
+++ presentation/src/main/java/com/android254/presentation/sessions/view/SessionsViewModel.kt
@@ -29,6 +29,8 @@ import com.android254.presentation.models.SessionsFilterOption
 import com.android254.presentation.sessions.mappers.toPresentationModel
 import com.android254.presentation.sessions.utils.SessionsFilterCategory
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.toLocalDate
@@ -43,34 +45,28 @@ data class Error(
 class SessionsViewModel @Inject constructor(
     private val sessionsRepo: SessionsRepo,
 ) : ViewModel() {
-    private val _sessions: MutableLiveData<List<SessionPresentationModel>> =
-        MutableLiveData(listOf())
-    private val _displayableSessions: MutableLiveData<List<SessionPresentationModel>> =
-        MutableLiveData(listOf())
-    private val _error: MutableLiveData<Error> = MutableLiveData(null)
-    private val _loading: MutableLiveData<Boolean> = MutableLiveData(false)
-    private val _empty: MutableLiveData<Boolean> = MutableLiveData(false)
-    private val _selectedFilterOptions: MutableLiveData<List<SessionsFilterOption>> =
-        MutableLiveData(
-            mutableListOf(),
-        )
-    private val _filterState: MutableLiveData<SessionsFilterState> = MutableLiveData(
-        SessionsFilterState(),
-    )
+    private val _sessions: MutableStateFlow<List<SessionPresentationModel>> = MutableStateFlow(emptyList())
+    private val _displayableSessions: MutableStateFlow<List<SessionPresentationModel>?> = MutableStateFlow(emptyList())
 
-    private val _selectedEventDate: MutableLiveData<EventDate> = MutableLiveData(
+    private val _error = MutableStateFlow<Error?>(null)
+    private val _loading = MutableStateFlow(false)
+    private val _empty  = MutableStateFlow(false)
+    private val _selectedFilterOptions: MutableStateFlow<List<SessionsFilterOption>> = MutableStateFlow(emptyList())
+    private val _filterState: MutableStateFlow<SessionsFilterState?> = MutableStateFlow(SessionsFilterState())
+
+    private val _selectedEventDate: MutableStateFlow<EventDate> = MutableStateFlow(
         EventDate(
-            LocalDate(2022, 11, 16),
+            LocalDate(year = 2023, monthNumber =  11, dayOfMonth = 16),
         ),
     )
 
-    val selectedEventDate: MutableLiveData<EventDate> = _selectedEventDate
+    val selectedEventDate = _selectedEventDate.asStateFlow()
+    var sessions = _displayableSessions.asStateFlow()
 
-    var sessions: LiveData<List<SessionPresentationModel>> = _displayableSessions
-    var error: LiveData<Error> = _error
-    var loading: LiveData<Boolean> = _loading
-    var empty: LiveData<Boolean> = _empty
-    var selectedFilterOptions: LiveData<List<SessionsFilterOption>> = _selectedFilterOptions
+    var error = _error.asStateFlow()
+    var loading = _loading.asStateFlow()
+    var empty = _empty.asStateFlow()
+    var selectedFilterOptions= _selectedFilterOptions.asStateFlow()
 
     init {
         viewModelScope.launch {
@@ -79,18 +75,16 @@ class SessionsViewModel @Inject constructor(
     }
 
     fun updateSelectedFilterOptionList(option: SessionsFilterOption) {
-        if (_selectedFilterOptions.value?.contains(option) == true) {
-            val index = _selectedFilterOptions.value?.indexOf(option)
-            if (index != null) {
-                _selectedFilterOptions.value =
-                    _selectedFilterOptions.value?.toMutableList()?.apply {
-                        removeAt(index)
-                    }
-            }
+        if (_selectedFilterOptions.value.contains(option)) {
+            val index = _selectedFilterOptions.value.indexOf(option)
+            _selectedFilterOptions.value =
+                _selectedFilterOptions.value.toMutableList().apply {
+                    removeAt(index)
+                }!!
         } else {
-            _selectedFilterOptions.value = _selectedFilterOptions.value?.toMutableList()?.apply {
+            _selectedFilterOptions.value = _selectedFilterOptions.value.toMutableList()?.apply {
                 add(option)
-            }
+            }!!
         }
 
         updateFilterState(option)
@@ -109,7 +103,7 @@ class SessionsViewModel @Inject constructor(
                 }?.toList()
                 _filterState.value = _filterState.value?.copy(
                     levels = newValue!!,
-                )
+                )!!
             }
             SessionsFilterCategory.Topic -> {
                 val newValue = _filterState.value!!.topics.toMutableList().apply {
@@ -122,7 +116,7 @@ class SessionsViewModel @Inject constructor(
                 }.toList()
                 _filterState.value = _filterState.value?.copy(
                     topics = newValue,
-                )
+                )!!
             }
             SessionsFilterCategory.Room -> {
                 val newValue = _filterState.value!!.rooms.toMutableList().apply {
@@ -135,7 +129,7 @@ class SessionsViewModel @Inject constructor(
                 }.toList()
                 _filterState.value = _filterState.value?.copy(
                     rooms = newValue,
-                )
+                )!!
             }
             SessionsFilterCategory.SessionType -> {
                 val newValue = _filterState.value!!.sessionTypes.toMutableList().apply {
@@ -162,7 +156,7 @@ class SessionsViewModel @Inject constructor(
                 result.data.let { sessionDomainModels ->
                     _sessions.value = sessionDomainModels?.map { sessionDomainModel ->
                         sessionDomainModel.toPresentationModel()
-                    }
+                    }!!
 
                     _displayableSessions.value =
                         sessionDomainModels?.map { sessionDomainModel ->
