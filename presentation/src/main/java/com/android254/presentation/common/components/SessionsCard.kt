d708caa8deb087b6c8d1e35793c4679acd6f514a
Harun Wangereka <harunwangereka@gmail.com>
2023-04-05 00:27:04 +0300

    fixing issues

--- presentation/src/main/java/com/android254/presentation/common/components/SessionsCard.kt
+++ presentation/src/main/java/com/android254/presentation/common/components/SessionsCard.kt
@@ -15,15 +15,28 @@
  */
 package com.android254.presentation.common.components
 
-import android.os.Build
-import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material.icons.rounded.StarOutline
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
@@ -46,6 +59,7 @@ import com.android254.presentation.sessions.view.SessionsViewModel
 import com.droidconke.chai.atoms.MontserratBold
 import com.droidconke.chai.atoms.MontserratSemiBold
 import kotlinx.coroutines.launch
+
 @Composable
 fun SessionsCard(
     session: SessionPresentationModel,
@@ -171,8 +185,10 @@ fun SessionTitleComponent(
         )
         IconButton(onClick = {
             scope.launch {
-                when (val result =
-                    viewModel.updateBookmarkStatus(session.remoteId, isStarred.value)) {
+                when (
+                    val result =
+                        viewModel.updateBookmarkStatus(session.remoteId, isStarred.value)
+                ) {
                     is ResourceResult.Empty -> {}
                     is ResourceResult.Error -> {
                     }
