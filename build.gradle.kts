e4dc51725ec8d261cbfffac1c30389604c39a59a
Harun Wangereka <harunwangereka@gmail.com>
2023-05-10 12:10:01 +0300

    updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ buildscript {
     }
 }
 plugins {
-    id("com.android.application") version "7.4.1" apply false
-    id("com.android.library") version "7.4.1" apply false
+    id("com.android.application") version "8.0.0" apply false
+    id("com.android.library") version "8.0.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("com.google.devtools.ksp") version "1.8.0-1.0.9" apply true
     kotlin("plugin.serialization") version "1.6.21"
