94b61deb298e749221e241230685cf643a9dc165
jacqui <gitaujaquiline@gmail.com>
2023-06-08 02:39:44 +0300

    Changed ResourceResult<List<Feed>> -> List<Feed>

--- domain/src/main/java/com/android254/domain/repos/FeedRepo.kt
+++ domain/src/main/java/com/android254/domain/repos/FeedRepo.kt
@@ -16,8 +16,7 @@
 package com.android254.domain.repos
 
 import com.android254.domain.models.Feed
-import com.android254.domain.models.ResourceResult
 
 interface FeedRepo {
-    suspend fun fetchFeed(): ResourceResult<List<Feed>>
+    suspend fun fetchFeed(): List<Feed>
 }
