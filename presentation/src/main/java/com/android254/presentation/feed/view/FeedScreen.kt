a797ae9450b16aaeab0247a7fa7bfa6a19c4da86
jacqui <gitaujaquiline@gmail.com>
2023-06-09 23:24:07 +0300

    Handling state in the FeedScreen.kt file

--- presentation/src/main/java/com/android254/presentation/feed/view/FeedScreen.kt
+++ presentation/src/main/java/com/android254/presentation/feed/view/FeedScreen.kt
@@ -16,26 +16,36 @@
 package com.android254.presentation.feed.view
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Error
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.android254.presentation.common.bottomsheet.BottomSheetScaffold
-import com.android254.presentation.common.bottomsheet.rememberBottomSheetScaffoldState
 import com.android254.presentation.common.components.DroidconAppBarWithFeedbackButton
 import com.android254.presentation.feed.FeedViewModel
 import com.droidconke.chai.ChaiDCKE22Theme
@@ -46,48 +56,61 @@ fun FeedScreen(
     navigateToFeedbackScreen: () -> Unit = {},
     feedViewModel: FeedViewModel = hiltViewModel()
 ) {
-    val bottomSheetScaffoldState = rememberBottomSheetScaffoldState()
+    val bottomSheetState = rememberSheetState()
     val scope = rememberCoroutineScope()
     feedViewModel.fetchFeed()
     val feedUIState = feedViewModel.viewState
-    BottomSheetScaffold(
-        sheetContent = {
-            FeedShareSection()
-        },
-        scaffoldState = bottomSheetScaffoldState,
-        topBar = {
-            DroidconAppBarWithFeedbackButton(
-                onButtonClick = {
-                    navigateToFeedbackScreen()
-                },
-                userProfile = "https://media-exp1.licdn.com/dms/image/C4D03AQGn58utIO-x3w/profile-displayphoto-shrink_200_200/0/1637478114039?e=2147483647&v=beta&t=3kIon0YJQNHZojD3Dt5HVODJqHsKdf2YKP1SfWeROnI"
-            )
-        },
-        sheetShape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp),
-        sheetElevation = 16.dp,
-        sheetPeekHeight = 0.dp
-    ) { paddingValues ->
+    if (bottomSheetState.isVisible) ModalBottomSheet(sheetState = bottomSheetState,
+        onDismissRequest = { scope.launch { bottomSheetState.hide() } }) {
+        FeedShareSection()
+    }
+    Scaffold(topBar = {
+        DroidconAppBarWithFeedbackButton(
+            onButtonClick = {
+                navigateToFeedbackScreen()
+            },
+            userProfile = "https://media-exp1.licdn.com/dms/image/C4D03AQGn58utIO-x3w/profile-displayphoto-shrink_200_200/0/1637478114039?e=2147483647&v=beta&t=3kIon0YJQNHZojD3Dt5HVODJqHsKdf2YKP1SfWeROnI"
+        )
+    }) { paddingValues ->
         Box(
             modifier = Modifier
-                .padding(paddingValues)
+                .padding(paddingValues = paddingValues)
                 .fillMaxSize()
         ) {
             when (feedUIState) {
-                is FeedUIState.Error -> {}
-                FeedUIState.Loading -> {}
+                is FeedUIState.Error -> {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Image(
+                            modifier = Modifier
+                                .width(50.dp)
+                                .height(50.dp),
+                            imageVector = Icons.Rounded.Error,
+                            contentDescription = "Error",
+                            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.error)
+                        )
+                        Text(text = feedUIState.message)
+                    }
+                }
+
+                FeedUIState.Loading -> {
+                    CircularProgressIndicator()
+                }
+
                 is FeedUIState.Success -> {
                     LazyColumn(
                         modifier = Modifier.testTag("feeds_lazy_column"),
                         verticalArrangement = Arrangement.spacedBy(4.dp)
                     ) {
                         items(feedUIState.feeds) { feedPresentationModel ->
-                            FeedComponent(modifier = Modifier.fillMaxWidth(), feedPresentationModel) {
+                            FeedComponent(
+                                modifier = Modifier.fillMaxWidth(), feedPresentationModel
+                            ) {
                                 scope.launch {
-                                    if (bottomSheetScaffoldState.bottomSheetState.isCollapsed) {
-                                        bottomSheetScaffoldState.bottomSheetState.expand()
-                                    } else {
-                                        bottomSheetScaffoldState.bottomSheetState.collapse()
-                                    }
+                                    bottomSheetState.show()
                                 }
                             }
                         }
