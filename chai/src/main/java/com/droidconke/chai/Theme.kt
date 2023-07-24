e15386ed7a88bc86fbf74c6579fa6815d33e7791
Hannah Olukoye <hannaholukoye@gmail.com>
2023-07-24 19:17:25 +0200

    Feat: Migrate all compose dependencies to convention plugin

--- chai/src/main/java/com/droidconke/chai/Theme.kt
+++ chai/src/main/java/com/droidconke/chai/Theme.kt
@@ -20,7 +20,11 @@ import android.content.Context
 import android.content.ContextWrapper
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
