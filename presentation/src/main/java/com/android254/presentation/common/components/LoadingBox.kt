afeb1f9d527a50da0b4229257f83c3c691ddbaec
robert-nganga <robertnganga220@gmail.com>
2023-09-08 16:21:22 +0300

    Add loading box

---
+++ presentation/src/main/java/com/android254/presentation/common/components/LoadingBox.kt
@@ -0,0 +1,39 @@
+package com.android254.presentation.common.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoadingBox(
+    height: Dp,
+    width: Dp = 0.dp,
+    widthRatio: Float? = null,
+    cornerRadius: Dp = 5.dp,
+    brush: Brush? = null
+){
+    Box(
+        modifier = Modifier
+            .customWidth(widthRatio, width)
+            .height(height)
+            .clip(RoundedCornerShape(cornerRadius))
+            .customBackground(brush)
+    )
+}
+
+
+fun Modifier.customBackground(brush: Brush?) = if (brush != null) background(brush)
+    else background(Color.LightGray.copy(alpha = 0.6f))
+
+fun Modifier.customWidth(ratio: Float?, width: Dp) = if (ratio!=null) fillMaxWidth(ratio)
+    else width(width)
