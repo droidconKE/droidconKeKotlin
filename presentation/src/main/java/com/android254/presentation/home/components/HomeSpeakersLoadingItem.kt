472f3a8d5a1743c868657815e3d58873cd961574
chepsi <evanschepsiror@gmail.com>
2023-09-15 16:17:48 +0300

    Fix: Enhance Lint

--- presentation/src/main/java/com/android254/presentation/home/components/HomeSpeakersLoadingItem.kt
+++ presentation/src/main/java/com/android254/presentation/home/components/HomeSpeakersLoadingItem.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 DroidconKE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.android254.presentation.home.components
 
 import androidx.compose.foundation.layout.Column
@@ -12,13 +27,12 @@ import androidx.compose.ui.unit.dp
 import com.android254.presentation.common.components.LoadingBox
 import com.android254.presentation.common.theme.DroidconKE2023Theme
 
-
 @Composable
-fun HomeSpeakersLoadingItem(){
+fun HomeSpeakersLoadingItem() {
     Column(
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 10.dp),
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
+    ) {
         LoadingBox(height = 80.dp, width = 80.dp)
         Spacer(modifier = Modifier.height(10.dp))
         LoadingBox(height = 20.dp, width = 60.dp)
