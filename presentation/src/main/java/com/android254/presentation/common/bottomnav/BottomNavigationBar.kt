91c4d4062edea24fcec55d065eaa015bf9c5ae59
Waitara <janewaitara99@gmail.com>
2023-10-29 15:25:29 +0300

    Updated the bottom navigation bar to match the designs

--- presentation/src/main/java/com/android254/presentation/common/bottomnav/BottomNavigationBar.kt
+++ presentation/src/main/java/com/android254/presentation/common/bottomnav/BottomNavigationBar.kt
@@ -15,11 +15,23 @@
  */
 package com.android254.presentation.common.bottomnav
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -27,42 +39,79 @@ import androidx.navigation.compose.rememberNavController
 import com.android254.presentation.common.navigation.Screens
 import com.android254.presentation.common.navigation.bottomNavigationDestinations
 import com.android254.presentation.common.theme.DroidconKE2023Theme
+import com.droidconke.chai.chaiColorsPalette
+import com.droidconke.chai.components.ChaiTextLabelSmall
 
 @Composable
 fun BottomNavigationBar(navController: NavHostController) {
-    BottomAppBar(containerColor = MaterialTheme.colorScheme.background) {
+    BottomAppBar(
+        modifier = Modifier
+            .background(MaterialTheme.chaiColorsPalette.bottomNavBorderColor)
+            .padding(top = 1.dp),
+        containerColor = MaterialTheme.chaiColorsPalette.bottomNavBackgroundColor
+    ) {
         val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentDestination = navBackStackEntry?.destination
 
         bottomNavigationDestinations.forEach { destination ->
             val selected =
                 currentDestination?.hierarchy?.any { it.route == destination.route } == true
-            NavigationBarItem(
-                selected = selected,
-                icon = {
-                    Icon(
-                        painter = painterResource(id = destination.icon),
-                        contentDescription = destination.title
-                    )
-                },
-                label = { Text(text = destination.title) },
-                alwaysShowLabel = true,
+
+            BottomNavItem(
+                isSelected = selected,
+                destination = destination,
                 onClick = {
                     navController.navigate(destination.route) {
                         launchSingleTop = true
                         restoreState = true
                         popUpTo(Screens.Home.route)
                     }
-                },
-                colors = NavigationBarItemDefaults.colors(
-                    selectedIconColor = MaterialTheme.colorScheme.primary,
-                    unselectedIconColor = MaterialTheme.colorScheme.onBackground,
-                    selectedTextColor = MaterialTheme.colorScheme.secondary,
-                    unselectedTextColor = MaterialTheme.colorScheme.onBackground,
-                    indicatorColor = MaterialTheme.colorScheme.background.copy(alpha = 0f)
-                )
+                }
             )
         }
+    }
+}
+
+@Composable
+fun RowScope.BottomNavItem(
+    isSelected: Boolean,
+    destination: Screens,
+    onClick: () -> Unit
+) {
+    val iconColor = if (isSelected) {
+        MaterialTheme.chaiColorsPalette.activeBottomNavIconColor
+    } else {
+        MaterialTheme.chaiColorsPalette.inactiveBottomNavIconColor
+    }
+
+    val textColor = if (isSelected) {
+        MaterialTheme.chaiColorsPalette.activeBottomNavTextColor
+    } else {
+        MaterialTheme.chaiColorsPalette.textNormalColor
+    }
+
+    Column(
+        modifier = Modifier
+            .weight(1f)
+            .fillMaxHeight()
+            .clickable(
+                onClick = onClick
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            painter = painterResource(id = destination.icon),
+            contentDescription = destination.title,
+            tint = iconColor
+        )
+        Spacer(modifier = Modifier.height(6.dp))
+
+        ChaiTextLabelSmall(
+            modifier = Modifier,
+            bodyText = destination.title,
+            textColor = textColor
+        )
     }
 }
 
