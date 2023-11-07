e6995615b06878363101952f9f48d59d8d82fbad
chepsi <evanschepsiror@gmail.com>
2023-11-07 12:10:53 +0300

    Fix: production bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,12 @@ android {
     buildTypes {
         debug {
             isDebuggable = true
+            isMinifyEnabled = true
             signingConfig = signingConfigs.getByName("debug")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
         release {
             isMinifyEnabled = true
