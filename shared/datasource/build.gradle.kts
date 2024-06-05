e582708a0a08896344a6a37b38f60260336e84c3
theophiluskibet <kibettheophilus@gmail.com>
2024-06-05 22:22:43 +0300

    migrate shared modules to use droidconke.multiplatform plugin

--- shared/datasource/build.gradle.kts
+++ shared/datasource/build.gradle.kts
@@ -1,32 +1,11 @@
 plugins {
-    kotlin("multiplatform")
-    id("com.android.library")
+    alias(libs.plugins.droidconke.multiplatform)
 }
 
 kotlin {
-
-    androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "17"
-            }
-        }
-    }
-
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {
-        it.binaries.framework {
-            baseName = "shared"
-            isStatic = true
-        }
-    }
-
     sourceSets {
-        val commonMain by getting{
-            dependencies{
+        val commonMain by getting {
+            dependencies {
 
             }
         }
@@ -40,12 +19,4 @@ kotlin {
 
 android {
     namespace = "ke.droidcon.kotlin.shared.datasource"
-    compileSdk = 34
-    defaultConfig {
-        minSdk = 24
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
 }
