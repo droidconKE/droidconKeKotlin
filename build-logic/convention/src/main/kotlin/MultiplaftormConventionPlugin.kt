a75bcffb0b30897fc3b39aa46bb7af5731acb7c9
theophiluskibet <kibettheophilus@gmail.com>
2024-06-05 22:21:17 +0300

    create MultiplaftormConventionPlugin.kt

---
+++ build-logic/convention/src/main/kotlin/MultiplaftormConventionPlugin.kt
@@ -0,0 +1,48 @@
+import com.android.build.gradle.LibraryExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.gradle.kotlin.dsl.configure
+
+class MultiplaftormConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("org.jetbrains.kotlin.multiplatform")
+                apply("com.android.library")
+
+                extensions.configure<KotlinMultiplatformExtension> {
+                    androidTarget {
+                        compilations.all {
+                            kotlinOptions {
+                                jvmTarget = "17"
+                            }
+                        }
+                    }
+
+                    listOf(
+                        iosX64(),
+                        iosArm64(),
+                        iosSimulatorArm64()
+                    ).forEach {
+                        it.binaries.framework {
+                            baseName = "shared"
+                            isStatic = true
+                        }
+                    }
+                }
+
+                extensions.configure<LibraryExtension> {
+                    compileSdk = 34
+                    defaultConfig {
+                        minSdk = 24
+                    }
+                    compileOptions {
+                        sourceCompatibility = JavaVersion.VERSION_17
+                    }
+                }
+            }
+        }
+    }
+}
