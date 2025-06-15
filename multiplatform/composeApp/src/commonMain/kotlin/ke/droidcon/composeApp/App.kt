d9af1b4d1a2300d9a2a35cbad6ea04adfb4844ac
Michael Ndiritu <ndiritumichael5@gmail.com>
2025-06-15 11:50:11 +0300

    add droidcon greeting message

--- multiplatform/composeApp/src/commonMain/kotlin/ke/droidcon/composeApp/App.kt
+++ multiplatform/composeApp/src/commonMain/kotlin/ke/droidcon/composeApp/App.kt
@@ -2,6 +2,7 @@ package ke.droidcon.composeApp
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -22,23 +23,20 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+
         Column(
             modifier = Modifier
                 .safeContentPadding()
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
+
                 val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+
+                     Text(greeting)
                 }
-            }
-        }
+
+
     }
 }
