bb41d3251bd2eecd9c3c229c65b1970bd95ced2e
Michael Ndiritu <ndiritumichael5@gmail.com>
2025-06-15 11:54:44 +0300

    add droidcon greeting message

--- multiplatform/composeApp/src/androidMain/kotlin/ke/droidcon/composeApp/Platform.android.kt
+++ multiplatform/composeApp/src/androidMain/kotlin/ke/droidcon/composeApp/Platform.android.kt
@@ -3,7 +3,7 @@ package ke.droidcon.composeApp
 import android.os.Build
 
 class AndroidPlatform : Platform {
-    override val name: String = "Android ${Build.VERSION.SDK_INT}"
+    override val name: String = "Android ${Build.VERSION.RELEASE}"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
