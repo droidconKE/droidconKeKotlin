4218c2b844d6e3d010383e390187ebf2ed115a2b
Michael Ndiritu <ndiritumichael5@gmail.com>
2025-06-15 11:55:02 +0300

    add droidcon greeting message

--- multiplatform/composeApp/src/commonMain/kotlin/ke/droidcon/composeApp/App.kt
+++ multiplatform/composeApp/src/commonMain/kotlin/ke/droidcon/composeApp/App.kt
@@ -1,21 +1,15 @@
 package ke.droidcon.composeApp
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import droidconke2023.multiplatform.composeapp.generated.resources.Res
-import droidconke2023.multiplatform.composeapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 
