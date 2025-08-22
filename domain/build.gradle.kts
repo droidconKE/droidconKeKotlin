b3a2c81d5d0177c55edf17c51ddde539ee4aff57
Wanjala Stephen <stephenwanjala145@gmail.com>
2025-08-22 11:09:14 +0300

    feat: Migrate domain module to Kotlin Multiplatform

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -14,14 +14,62 @@
  * limitations under the License.
  */
 plugins {
-    alias(libs.plugins.droidconke.android.library)
-    alias(libs.plugins.droidconke.android.library.jacoco)
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
 }
 
-android {
-    namespace = "ke.droidcon.kotlin.domain"
-}
+kotlin {
 
-dependencies {
-    implementation(libs.kotlinx.coroutines.core)
+    // Target declarations - add or remove as needed below. These define
+    // which platforms this KMP module supports.
+    // See: https://kotlinlang.org/docs/multiplatform-discover-project.html#targets
+    androidLibrary {
+        namespace ="ke.droidcon.kotlin.domain"
+        compileSdk = 36
+        minSdk = 24
+
+    }
+
+    // For iOS targets, this is also where you should
+    // configure native binary output. For more information, see:
+    // https://kotlinlang.org/docs/multiplatform-build-native-binaries.html#build-xcframeworks
+
+    // A step-by-step guide on how to include this library in an XCode
+    // project can be found here:
+    // https://developer.android.com/kotlin/multiplatform/migrate
+    val xcfName = "domain"
+
+    iosX64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    iosArm64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    iosSimulatorArm64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    // Source set declarations.
+    // Declaring a target automatically creates a source set with the same name. By default, the
+    // Kotlin Gradle Plugin creates additional source sets that depend on each other, since it is
+    // common to share sources between related targets.
+    // See: https://kotlinlang.org/docs/multiplatform-hierarchy.html
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(libs.kotlin.stdlib)
+                implementation(libs.kotlinx.coroutines.core)
+                // Add KMP dependencies here
+            }
+        }
+    }
+
 }
